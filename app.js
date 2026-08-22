3dbde1bfd0fe4ed14b0a75337b43a44f248a7c22
thomyzento-glitch <thomyzento@gmail.com>
2026-08-22 15:45:42 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const GOOGLE_PENDING_KEY="ecodata_google_pending_v1";
    ========================= */
 const GOOGLE_SHEETS_CONFIG = {
   endpoint: "https://script.google.com/macros/s/AKfycbxeOsdzWLDPQW6kDrEGdIf19QuPq8QYLDSttAXMz7XkxEhvPVMLGBRjl9D7rDjl4aIl/exec",
-  enabled: true
+  enabled: false
 };
 
 /* =========================
